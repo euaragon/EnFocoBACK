b7f507d03a4477e9e58d00f6e6a59b2f8d21e644
= <=>
2025-03-11 10:19:34 -0300

    todo solucionado

--- obj/Release/net8.0/EnFoco_new.AssemblyInfo.cs
+++ obj/Release/net8.0/EnFoco_new.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnFoco_new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e7737ffab8812b5cf6b2a3b00d8615b7df06bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc949c2e62f1141d0051b9d82e336a9393914634")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnFoco_new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnFoco_new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
